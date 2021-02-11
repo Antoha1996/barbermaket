b7629f346a5e5b0b00292b9c758af8d2d9b098d9
Antoha1996 <kalilinin@gmail.com>
2021-02-11 18:31:12 +0200

    gulp

---
+++ app/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	// $('body').hide()
+})
+
+//swiper-gallery
+new Swiper('.swiper-container', {
+	navigation: {
+		nextEl: '.swiper-btn-next',
+		prevEl: '.swiper-btn-prev'
+	},
+});
